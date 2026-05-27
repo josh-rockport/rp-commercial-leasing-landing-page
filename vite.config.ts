7d79c4e6a3a8bdf1ec0acbb518ef2efb0b13008a
josh-rockport <josh@rockportgroup.ca>
2026-05-27 15:58:15 -0500

    CNAME Added

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rp-commercial-leasing-landing-page/',
+  base: '/',
   css: {
     preprocessorOptions: {
       scss: {
